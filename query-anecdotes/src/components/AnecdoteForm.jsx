43cdab0c968347828d66f2a1f449423d99a2aa6e
ivanuria <ivan.uria@gmail.com>
2024-08-08 17:51:37 +0200

    Ex 6.24

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -7,10 +7,18 @@ const AnecdoteForm = () => {
   const queryClient = useQueryClient()
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
-    onSuccess: (newAnecdote) => {
+    onSuccess: newAnecdote => {
       const anecdotes = queryClient.getQueryData(['anecdotes'])
       queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
       notificationDispatch(createNotificationAction(`New Anecdote created: '${newAnecdote.content}'`))
+    },
+    onError: (error) => {
+      console.log(error)
+      if (error.response && error.response.data && error.response.data.error === 'too short anecdote, must have length 5 or more') {
+        notificationDispatch(createNotificationAction(`Anecdote must be at least 5 characters long`, 'ERROR'))
+      } else {
+        notificationDispatch(createNotificationAction(`Something's wrong with the server`, 'ERROR'))
+      }
     }
   })
 
